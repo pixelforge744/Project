9f275eaf8f08bb715c97392d6d580b9bf765aac3
Pujak <goyalpujak@gmail.com>
2026-07-30 17:11:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter,20);
+
+        }
+
+        else{
+
+            counter.innerText = target + "+";
+
+        }
+
+    }
+
+    updateCounter();
+
+});
